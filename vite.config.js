6e063e55ed25b9d0cb1ced509f44ceaaf8458a3c
Benbinbin <benthomsonbin@gmail.com>
2023-05-03 10:48:51 +0800

    :arrow_up: update node version

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' // Node 14 & 16
+// import manifest from './manifest.json' // Node 14 & 16
+import manifest from './manifest.json' assert { type: 'json' } // Node >=17
 
 // https://vitejs.dev/config/
 export default defineConfig({
